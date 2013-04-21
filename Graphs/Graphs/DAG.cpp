c894431b00ebf90882f504e8d0805dc58569b11a
Endurance Idehen <jeidehen@gmail.com>
2013-04-21 12:12:32 -0500

    Directed Acyclic Graph - Intro

---
+++ Graphs/Graphs/DAG.cpp
@@ -0,0 +1,26 @@
+/*******************
+Written by: Endurance Idehen
+Directed Acyclic Graph
+Operations:
+Traversal (print all items)
+Find/Search
+Insert
+Delete
+*******************/
+
+#include "stdafx.h"
+#include "DAG.h"
+
+
+DAG::DAG(void)
+{
+}
+
+
+DAG::~DAG(void)
+{
+}
+
+void DAG::insert(gNode g){
+
+}
---
+++ Graphs/Graphs/DAG.h
@@ -0,0 +1,14 @@
+#pragma once
+class DAG
+{
+	struct gNode{
+
+	};
+
+
+public:
+	DAG(void);
+	~DAG(void);
+	void insert(gNode node);
+};
+
